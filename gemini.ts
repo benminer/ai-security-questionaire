d5a3c99aafd5341280210dcc84a4df5a61c422d8
Ari Spikerman <aspikerman@scope3.com>
2025-01-02 17:26:04 -0500

    answer questions

--- gemini.ts
+++ gemini.ts
@@ -4,6 +4,9 @@ import { params } from "@ampt/sdk";
 
 const genAI = new GoogleGenerativeAI(params("GEMINI_API_KEY"));
 const gemini = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const tunedModel = genAI.getGenerativeModel({
+  model: "tunedModels/trainingcsv1-ih2fyjh9ms5l",
+});
 
 export interface AnalyseDataRequest extends Request {
   body: {
@@ -14,22 +17,50 @@ export interface AnalyseDataRequest extends Request {
 export const analyseData = async (req: AnalyseDataRequest, res: Response) => {
   try {
     const data = req.body.text;
+    console.log("\nraw input data:", data)
 
     // Send the prompt with the JSON data to the model
     const result = await gemini.generateContent([
-      `${data}. Extract only the questions in a JSON format with a "question" prop. Return only the JSON data`,
+      `${data}. Extract only the questions in a JSON format as an array of strings. Return only the JSON data`,
     ]);
 
-    const resultJson = result.response.text();
+    const resultText = result.response.text();
 
     const questions = JSON.parse(
-      resultJson.replace("```json", "").replace("```", "")
+      resultText.replace("```json", "").replace("```", "")
     );
 
-    res.send({ questions, error: null });
-  } catch (error) {
-    console.error("Error generating insights:", error);
+    console.log("\nextracted questions:", questions)
 
-    res.send({ insights: null, error: "Error" });
+    return answerQuestions(questions, res);
+  } catch (error) {
+    console.error("Error extracting questions:", error);
+
+    return res.status(500).send({ questions: null, error });
+  }
+};
+
+export const answerQuestions = async (questions: object, res: Response) => {
+  try {
+    const prompt = "Answer the following questions. Respond in JSON format. Add the answer to each JSON object as a key-value pair with the key `answer`. Include questions as provided. Return only the JSON data.";
+
+    const modelInput = `${prompt} The questions are:\n\n${JSON.stringify(questions)}`;
+
+    console.log("\n\nmodelInput\n", modelInput)
+
+    const result = await tunedModel.generateContent(modelInput);
+
+    const resultText = result.response.text();
+    console.log("\n tuned model result text:", resultText)
+    const questionAnswers = JSON.parse(
+      resultText.replace("```json", "").replace("```", "")
+    )
+    console.log("\ntuned model response:", questionAnswers)
+
+
+    return res.status(200).send({ questions: questionAnswers, error: null });
+  } catch (error) {
+    console.error("Error answering questions:", error);
+    return res.status(500).send({ questions: null, error});
   }
 };
